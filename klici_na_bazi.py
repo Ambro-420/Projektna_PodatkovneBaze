c60dc79e3eb4a75d71e16180132b4149591c376e
ambro <ap40514@student.uni-lj.si>
2025-12-27 16:34:31 +0100

    dodal .py datoteko za pridobivanje podatkov iz baze

---
+++ klici_na_bazi.py
@@ -0,0 +1,16 @@
+import sqlite3 as dbapi
+
+# py datoteka za pridobivanje podatkov iz podatkovne baze
+
+lokacija = "/home/ambro/Desktop/Projektna_PodatkovneBaze/baza.db"
+povezava = dbapi.connect(lokacija)
+kazalec = povezava.cursor()
+
+# poskusna poizvedba
+kazalec.execute("SELECT * FROM product")
+rezultati = kazalec.fetchall() # .fetchall vre vrstice
+
+for vrstica in rezultati:
+    print(vrstica)
+kazalec.close()
+povezava.close()
